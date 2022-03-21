0d32b7a91a2830cebcc8a791d40dcd0c461bd81a
Chris-Mitchell90 <chris.mitchell1@hotmail.co.uk>
2022-03-21 18:27:55 +0000

    add searchenter function

--- src/controllers/AddShow/searchenter.ts
+++ src/controllers/AddShow/searchenter.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 import { Request, Response } from 'express';
 import TVShowSnippet from '../../models/tv-show-snippet';
-
+import * as dotenv from 'dotenv';
+dotenv.config();
+const apiUrl = 'https://api.themoviedb.org/3/';
+const APIKEY = process.env.API_KEY;
 
 /*
  * function to return 20 matches for search bar entry with manual enter
@@ -11,7 +14,16 @@ export const searchEnter = async (req: Request, res: Response): Promise<void> =>
 	try {
 		const search: string = req.body.search;
 		const page: number = req.body.page;
-		const {data} = await axios.get(`${apiUrl}trending/tv/week?api_key=${APIKEY}&page=${page}`);
+		const { data } = await axios.get(`${apiUrl}search/tv?api_key=${APIKEY}&page=${page}&query=${search}&include_adult=false`);
+		let tvShows: TVShowSnippet[] = [];
+		for (let i = 0; i < data.results.length; i++) {
+			tvShows.push({
+			  name: data.results[i].name,
+				TMDB_show_id: data.results[i].id,
+				poster_path: data.results[i].poster_path,
+				first_air_date: data.results[i].first_air_date
+      })
+		};
 		res.status(200);
 		res.send(tvShows);
 	} catch (e) {
