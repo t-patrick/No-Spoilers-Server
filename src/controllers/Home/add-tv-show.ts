e8d0836dce97e82df26fba3c95982599b0f6645d
Chris-Mitchell90 <chris.mitchell1@hotmail.co.uk>
2022-03-25 18:24:39 +0000

    refactor add tv show to take from database if already entered there

--- src/controllers/Home/add-tv-show.ts
+++ src/controllers/Home/add-tv-show.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import * as dotenv from 'dotenv';
 import { Request, Response } from 'express';
+import FullTVShow from '../../models/full-tv-show';
 dotenv.config();
 import UserTVShow from '../../models/user-tv-show';
 const apiUrl = 'https://api.themoviedb.org/3/';
@@ -24,12 +25,46 @@ export const addTVShow = async (req: Request, res: Response): Promise<void> => {
         'TV show already exists in your TV show list. Please choose a new show instead.'
       );
       return;
-    }
+		}
+		let newTVShow: UserTVShow;
+		const fullTVShow: TVShow | null = await FullTVShow.findOne({ TMDB_show_id: TMDB_show_id });
+		if (fullTVShow) {
+			if (fullTVShow.seasons) {
+				newTVShow = {
+					userId: userId,
+					TMDB_show_id: TMDB_show_id,
+					name: fullTVShow.name,
+					poster_path: fullTVShow.poster_path,
+					current_poster_path: fullTVShow.seasons[0].poster_path,
+					isCompleted: false,
+					episodeIdUpTo: 0,
+					episodeCodeUpTo: '',
+					episodeCodeNext: 's1e1',
+					episodesWatchedSoFar: 0,
+				};
+			} else {
+				newTVShow = {
+					userId: userId,
+					TMDB_show_id: TMDB_show_id,
+					name: fullTVShow.name,
+					poster_path: fullTVShow.poster_path,
+					isCompleted: false,
+					episodeIdUpTo: 0,
+					episodeCodeUpTo: '',
+					episodeCodeNext: 's1e1',
+					episodesWatchedSoFar: 0,
+				};
+			}
+			await UserTVShow.create(newTVShow);
+			res.status(200);
+			res.send(newTVShow);
+			return;
+		}
     const { data }: AxiosTVShow = await axios.get(
       `${apiUrl}tv/${TMDB_show_id}?api_key=${APIKEY}`
 		);
 		let firstSeason: AxiosSeason[];
-		let newTVShow: UserTVShow;
+
 		if (data.seasons) {
 			firstSeason = data.seasons.filter(season => season.season_number === 1);
 			newTVShow = {
